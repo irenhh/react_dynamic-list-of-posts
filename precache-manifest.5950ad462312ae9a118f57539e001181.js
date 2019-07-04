aa36f817106faced1be7c1e48bbb15c267f93236
Iryna <iriskag@ukr.net>
2019-07-04 22:59:08 +0300

    Updates

---
+++ precache-manifest.5950ad462312ae9a118f57539e001181.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54f0f0acf9fbbc9863dcdb897fac1bb5",
+    "url": "/react_dynamic-list-of-posts/index.html"
+  },
+  {
+    "revision": "d3d4231c27c1ec0f09a4",
+    "url": "/react_dynamic-list-of-posts/static/css/main.211f59fb.chunk.css"
+  },
+  {
+    "revision": "d565fbb3a70e24f55c84",
+    "url": "/react_dynamic-list-of-posts/static/js/2.7cd874ad.chunk.js"
+  },
+  {
+    "revision": "d3d4231c27c1ec0f09a4",
+    "url": "/react_dynamic-list-of-posts/static/js/main.419a9f23.chunk.js"
+  },
+  {
+    "revision": "fc988510490290486853",
+    "url": "/react_dynamic-list-of-posts/static/js/runtime~main.69709ad2.js"
+  }
+]);
